2f9e27d814b6640e2d05739ecc7d5759e1969915
ImYosrii <yosrisaed99@gmail.com>
2025-11-10 13:28:43 -0700

    init commit

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,25 @@
+'use client';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+export default function CanvasComponent() {
+    const cameraSettings = {
+        fov: 45,
+        near: 0.1,
+        far: 1000,
+        position: [0, 0, 5],
+    }
+    return (
+        <div className="canvas-container">
+            <Canvas
+                camera={cameraSettings}
+            >
+                <OrbitControls/>
+                <mesh>
+                    <boxGeometry/>
+                    <meshBasicMaterial color="red" />
+                </mesh>
+            </Canvas>
+        </div>
+    )
+}
